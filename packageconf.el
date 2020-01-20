fef92b4b671c12c876faeb837f5e622605ed366e
Nicholas Lantz <nlantz@pm.me>
2020-01-20 14:46:48 -0700

    Added Org capture todo change if currently in a projectile project.

--- packageconf.el
+++ packageconf.el
@@ -13,11 +13,6 @@
   :after slime
   :custom (slime-contribs (append '(slime-fancy) slime-contribs)))
 
-(use-package projectile
-  :bind
-  ("C-c p" . projectile-command-map)
-  :config (projectile-mode))
-
 (use-package org
   :bind (("C-c c" . org-capture)
 	 ("C-c a" . org-agenda))
@@ -25,25 +20,24 @@
   :custom
   (org-default-notes-file "~/org/todo.org")
   (org-capture-templates `(("t" "Task" entry
-			    (file+headline "" "Tasks")
+			    (file+headline (lambda () (if (projectile-project-root)
+							  (concat (projectile-project-root) "todo.org")
+							org-default-notes-file))
+					   "Tasks")
 			    "* TODO %?\n  %u\n  %i\n")
 			   ("a" "Appointment" entry
 			    (file+headline "" "Appointments")
-			    "* TODO %?\n  %^T\n  %i\n %^{Location}p\n ")
-			   ("b" "item to Buy" item
-			    (file+headline "~/org/tobuy.org" "Next")
-			    "- %?\n")
-			   ("m" "bookMark" item
-			    ;; This is kind of tricky...
-			    (file+function "~/org/bookmarks.org" (lambda ()
-								   (let ((name
-									  (read-from-minibuffer "Book Name: ")))
-								     (search-forward name))))
-			    " - %? :: \n")))
+			    "* TODO %?\n  %^T\n  %i\n %^{Location}p\n ")))
   (org-agenda-todo-ignore-timestamp 'all)
   (org-agenda-todo-ignore-deadlines 'near)
   (org-agenda-todo-ignore-scheduled 't)
   (org-agenda-todo-list-sublevels nil))
+
+(use-package projectile
+  :after org
+  :bind
+  ("C-c p" . projectile-command-map)
+  :config (projectile-mode +1))
 
 (use-package eshell
   :bind ("C-c e" . eshell))
